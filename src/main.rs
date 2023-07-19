8793736a774141d58ef9844b3726d161db85e415
Markus Rosenberg <markus.rosenberg@softwareone,com>
2023-07-19 10:34:34 +0200

    implemented base game + unfinished win detection

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,86 @@
 use std::time::Duration;
 
-use sdl2::{pixels::Color, rect::Rect};
+use sdl2::{pixels::Color, rect::Rect, event::Event, keyboard::Keycode, mouse::MouseButton};
 
 fn main() {
-    let width: u32 = 500;
-    let height: u32 = 500;
+    let size: u32 = 600;
+    let box_size: u32 = 50;
+    let mut board: [[i32; 3]; 3] = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+    let mut player = false;
+    let mut end = false;
 
     let context = sdl2::init().unwrap();
     let video_subsystem = context.video().unwrap();
 
-    let window = video_subsystem.window("Hello, World!", width, height)
+    let window = video_subsystem.window("Tic Tac Toe", size, size)
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
 
-    canvas.clear();
-    canvas.set_draw_color(Color::RGB(30, 30, 30));
-    canvas.fill_rect(Rect::new(0, 0, width, height)).unwrap();
+    let mut event_pump = context.event_pump().unwrap();
 
-    canvas.set_draw_color(Color::RGB(200, 200, 200));
-    canvas.fill_rect(Rect::new((width / 3) as i32, 0, 2, height)).unwrap();
-    canvas.fill_rect(Rect::new(((width / 3) * 2) as i32, 0, 2, height)).unwrap();
+    'running: loop {
+        canvas.clear();
 
-    canvas.fill_rect(Rect::new(0, (width / 3) as i32, width, 2)).unwrap();
-    canvas.fill_rect(Rect::new(0, ((width / 3) * 2) as i32, width, 2)).unwrap();
-    
-    canvas.present();
-    std::thread::sleep(Duration::new(3, 0));
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                Event::MouseButtonDown { mouse_btn: MouseButton::Left, x, y, .. } => {
+                    if end == true {continue;}
+                    
+                    let symbol = if player {1} else {2};
+                    let i = (y / (size as i32 / 3)) as usize;
+                    let j = (x / (size as i32 / 3)) as usize;
+                    if board[i][j] == 0{
+                        board[i][j] = symbol;
+                        
+                        for row in board {
+                            if row.iter().min() == row.iter().max() && row.iter().min() != Some(&0){
+                                println!("Player {} won!", player as i32 + 1);
+                                end = true;
+                            }
+                        }
+
+                        player = !player;
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        //Background
+        canvas.set_draw_color(Color::RGB(30, 30, 30));
+        canvas.fill_rect(Rect::new(0, 0, size, size)).unwrap();
+
+        //Board
+        canvas.set_draw_color(Color::RGB(200, 200, 200));
+        canvas.fill_rect(Rect::new((size / 3) as i32, 0, 2, size)).unwrap();
+        canvas.fill_rect(Rect::new(((size / 3) * 2) as i32, 0, 2, size)).unwrap();
+
+        canvas.fill_rect(Rect::new(0, (size / 3) as i32, size, 2)).unwrap();
+        canvas.fill_rect(Rect::new(0, ((size / 3) * 2) as i32, size, 2)).unwrap();
+
+        for (i, row) in board.iter().enumerate() {
+            for (j, element) in row.iter().enumerate() {
+                let x = j as i32 * (size as i32 / 3) + (size as i32 / 3) / 2 - box_size as i32 / 2;
+                let y = i as i32 * (size as i32 / 3) + (size as i32 / 3) / 2 - box_size as i32 / 2;
+                
+                if *element == 0 {continue;}
+                else if *element == 1 {
+                    canvas.set_draw_color(Color::RGB(0, 200, 0));
+                }
+                else if *element == 2 {
+                    canvas.set_draw_color(Color::RGB(0, 0, 200));
+                }
+                canvas.fill_rect(Rect::new(x, y, box_size, box_size)).unwrap();
+            }
+        }
+        
+        canvas.present();
+        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
